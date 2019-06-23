bda6bda9349e6bf417e093728fff1996a036f74b
Santiago Enciso <whereispolaris@gmail.com>
2019-06-22 23:43:44 -0500

    Removed API fuction, added object with array of questions, created function that renders questions  and answers to page.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,74 @@
 // Trivia Game API:  https://opentdb.com/api.php?amount=20&category=23&difficulty=easy&type=multiple
-$.ajax({
-    method: "GET",
-    url: "https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple",
-    success: function (questions) {
-        console.log('Success', questions);
-        // Question
-        console.log('Question: ', questions.results[0].question);
-        // Correct Answer
-        console.log('Correct Answer: ', questions.results[0].correct_answer);
-        // Incorrect Answers
-        console.log('Incorrect Answers: ', questions.results[0].incorrect_answers);
+
+triviaQuestions = [
+    {
+        questionText: "What is the capital of China?",
+        answers: [
+            { letterChoice: "a", answerText: "Bangkok", correct: false },
+            { letterChoice: "b", answerText: "Tokyo", correct: false },
+            { letterChoice: "c", answerText: "Hong Kong", correct: false },
+            { letterChoice: "d", answerText: "Beijing", correct: true },
+        ]
+    },
+    {
+        questionText: "What country has the most natural lakes?",
+        answers: [
+            { letterChoice: "a", answerText: "Canada", correct: true },
+            { letterChoice: "b", answerText: "India", correct: false },
+            { letterChoice: "c", answerText: "United States", correct: false },
+            { letterChoice: "d", answerText: "Australia", correct: false },
+        ]
+    },
+    {
+        questionText: "In what country can you visit Machu Picchu?",
+        answers: [
+            { letterChoice: "a", answerText: "Colombia", correct: false },
+            { letterChoice: "b", answerText: "Chile", correct: false },
+            { letterChoice: "c", answerText: "Peru", correct: true },
+            { letterChoice: "d", answerText: "Bolivia", correct: false },
+        ]
+    },
+
+];
+
+// ==== Global Variables ====
+
+
+// This fuction generates the question and answer from the triviaQuestions array 
+function questionGenerator(questionNumber) {
+
+    var question = $("<h4>");
+    // Generate question element
+    question.addClass("question");
+    question.text(triviaQuestions[questionNumber].questionText);
+    $("#questionDisplay").append(question);
+
+    // Array of answers 
+    var ansArray = triviaQuestions[questionNumber].answers;
+
+    // Generate Loop That Adds Answers to page
+    for (var a = 0; a < ansArray.length; a++) {
+        // var answerRadio = $("<h5>");
+        var answerRadio = $("<input>");
+        answerRadio.attr("type", "radio");
+        answerRadio.attr("name", "answers")
+        answerRadio.attr("id", ansArray[a].letterChoice);
+        var answerLabel = $("<label>");
+        answerLabel.attr("for", ansArray[a].letterChoice);
+        answerLabel.text(ansArray[a].answerText);
+        $("#answersDisplay").append(answerRadio, " ", answerLabel, "<br>");
     }
+}
+
+questionGenerator(0);
+
+$("input[name='answers']").on("click", function () {
+    console.log("radio button clicked");
+
+
 });
+
+
 
 // === PSEUDOCODE ===
 
@@ -29,6 +86,7 @@ $.ajax({
 
 
 // === TO DO ===
-// Build Layout
+// Build Layout - DONE
+// Create Questions array
 // Build Timer
 // Go over slideshow
