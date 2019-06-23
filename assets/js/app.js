781143453dd9e91619559abc340f18bb56484b54
Santiago Enciso <whereispolaris@gmail.com>
2019-06-22 20:10:05 -0500

    Pseudocoded game workflow, added To Do notes.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,15 +2,33 @@
 $.ajax({
     method: "GET",
     url: "https://opentdb.com/api.php?amount=20&category=23&difficulty=easy&type=multiple",
-    success: function (data) {
-        console.log('Success', data);
+    success: function (questions) {
+        console.log('Success', questions);
         // Question
-        console.log('Question: ', data.results[0].question);
+        console.log('Question: ', questions.results[0].question);
         // Correct Answer
-        console.log('Correct Answer: ', data.results[0].correct_answer);
+        console.log('Correct Answer: ', questions.results[0].correct_answer);
         // Incorrect Answers
-        console.log('Incorrect Answers: ', data.results[0].incorrect_answers);
+        console.log('Incorrect Answers: ', questions.results[0].incorrect_answers);
     }
 });
 
 // === PSEUDOCODE ===
+
+// DOCUMENT READY
+    // Show page that only has 'start game' button
+
+// Start Game Button - ON CLICK
+    // Start Timer
+    // Display Questions
+
+// TIMER ENDS
+    // Count Correct Answers
+    // Count Incorrect Answers
+    // Count Unanswered Questions
+
+
+// === TO DO ===
+// Build Layout
+// Build Timer
+// Go over slideshow
