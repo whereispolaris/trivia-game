5867bb4e95d45c0e350db5b136fdb9ae10c5ec6c
Santiago Enciso <whereispolaris@gmail.com>
2019-06-24 13:02:11 -0500

    Added comments to rewrite the questionGenerator function.

--- assets/js/app.js
+++ assets/js/app.js
@@ -156,5 +156,54 @@ questionGenerator(questionNumber);
 // === TO DO ===
 // Build Layout - DONE
 // Create Questions array - DONE
-// Build Timer
-// Go over slideshow
+// Build Timer - DONE - 
+// Go over slideshow - DONE
+// Rebuild question container
+
+
+// === Question ===
+
+/*
+//var questionRowDiv = $("<div>"); 
+// questionRowDiv.addClass("row"); 
+ <div class="row"> 
+    // var questionBox = $("<div>");
+    // questionBox.addClass("card col-md-8 questionBox");
+    <div class="card col-md-8 questionBox">
+        // var questionDisplay = $("<div>");
+        // questionDisplay.addClass("card-header");
+        // questionDisplay.attr("id", "questionDisplay");
+        <div class="card-header" id="questionDisplay">
+            <!-- This is where the question goess -->
+        </div>
+        // var cardBody = $("<div>");
+        // cardBody.addClass("card-body");
+        <div class="card-body">
+            // var answerDisplay = $("<form>");
+            // answerDisplay.attr("answersDisplay");
+            <form id="answersDisplay">
+                <!-- This is where the answers go -->
+            </form>
+        </div>
+    </div>
+    // var counterBox = $("<div>");
+    // counterBox.addClass("card col-md-3 counterBox");
+    <div class="card col-md-3 counterBox">
+        // var counterDisplay = $("<div>");
+        // counterDisplay.addClass("card-header");
+        // counterDisplay.attr("id", "counterDisplay");
+        <div class="card-header" id="counterDisplay">
+            <!-- This is where the timer will go -->
+            <h4>Timer: <span id="show-number"></span> </h4>
+        </div>
+        <div class="card-body">
+            <h6>Correct Answers: 3</h6>
+            <h6>Incorrect Answers: 1 </h6>
+            <h6>Unanswered Questions: 0</h6>
+        </div>
+    </div>
+</div> 
+
+*/
+
+
